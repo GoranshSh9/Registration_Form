c643add6d111e9b202266be8d935a6a529bf7455
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-21 01:17:11 +0530

    update

--- index.js
+++ index.js
@@ -8,6 +8,28 @@ dotenv.config();
 
 const port = process.env.Port || 3000;
 
+const username = process.env.MONGODB_USERNAME;
+const password = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(`mongodb+srv://${username}:${password}@cluster0.ss1csj5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+    useNewUrlParser : true,
+    useUnifiedTopoloy : true,
+});
+
+const registrationSchema = new 
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/register", (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+    } catch (error) {
+        
+    }
+})
+
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
 })
