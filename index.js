e02f36582634ec881ce885a1f2b9af5dedc426cf
GoranshSh9 <118844948+GoranshSh9@users.noreply.github.com>
2024-07-23 00:28:59 +0530

    Updates

--- index.js
+++ index.js
@@ -35,18 +35,23 @@ app.post("/register", async (req, res) => {
     try {
         const {name, email, password} = req.body;
 
-        const registrationData = new Registration({
-            name,
-            email,
-            password
-        });
+        const existingUser = await Registration.findOne({email : email});
+        if (!existingUser){
+            const registrationData = new Registration({
+                name,
+                email,
+                password,
+            });            
         await registrationData.save();
         res.redirect("/success");
+    } else {
+        alert("User already exist");
+        res.redirect("/error");
     } catch (error) {
         console.log(error);
         res.redirect("error");
     }
-})
+        });
 
 app.get("/success", (req, res)=>{
     res.sendFile(__dirname+"/success.html");
